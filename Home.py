c81ecc3b07c8cb9f7ff59816ef5ad6ce4f46691a
Teja Kiran Chadalavada <Teja.Chadalavada@hotmail.com>
2018-04-07 21:34:41 +0530

    Added skeleton for Models

---
+++ Home.py
@@ -0,0 +1,9 @@
+import os
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+ os.path.join(basedir,'powercharting.db')
+db = SQLAlchemy(app)
---
+++ Models.py
@@ -0,0 +1,31 @@
+from Home import db
+
+class devices(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.Text,nullable=False)
+    sonoff_ip = db.Column(db.Text,nullable=False)
+
+    def __repr__(self):
+        return self.name
+
+class logtable(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    logtime_year=db.Column(db.Integer,nullable=False)
+    logtime_month=db.Column(db.Integer,nullable=False)
+    logtime_date=db.Column(db.Integer,nullable=False)
+    logtime_hour=db.Column(db.Integer,nullable=False)
+    logtime_minute=db.Column(db.Integer,nullable=False)
+    logtime_second=db.Column(db.Integer,nullable=False)
+    devices_id = db.Column(db.Integer, db.ForeignKey('devices.id'), nullable=False)
+    devices_name = db.Column(db.Text, db.ForeignKey('devices.name'), nullable=False)
+    devices_sonoff_ip = db.Column(db.Text, db.ForeignKey('devices.sonoff_ip'), nullable=False)
+    power_status = db.Column(db.Boolean,nullable=False)
+    wifi_strength = db.Column(db.Float,nullable=False)
+    vcc = db.Column(db.Float)
+    period = db.Column(db.Integer)
+    power = db.Column(db.Float,nullable=False)
+    factor = db.Column(db.Float)
+    voltage = db.Column(db.Float)
+    current = db.Column(db.Float)
+    kwh_sofar_today = db.Column(db.Float,nullable=False)
+    kwh_yesterday = db.Column(db.Float,nullable=False)
