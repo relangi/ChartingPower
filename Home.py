8b1700cbb9d6d5f70d9e6ff74106073605e2973d
Teja Kiran Chadalavada <Teja.Chadalavada@hotmail.com>
2018-04-08 23:17:00 +0530

    Tested single chart of day vs kwh. lg

--- Home.py
+++ Home.py
@@ -1,4 +1,4 @@
-import os
+import os,Models
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
@@ -10,7 +10,9 @@ db = SQLAlchemy(app)
 
 @app.route('/dashboard')
 def dashboard():
-    return ('dashboard.html')
+    labels_data = [str(val.logtime_year)+'/'+str(val.logtime_month)+'/'+str(val.logtime_date) for val in Models.daily_summary.query.limit(100).all()]
+    values_data = [float(val.kwh_ontheday) for val in Models.daily_summary.query.limit(100).all()]
+    return render_template('linechart.html',title='Last 100 days of Power usage',labels=labels_data, values=values_data,max=10)
 
 if __name__ == '__main__':
     app.run()
