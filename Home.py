ac99beaf8a97c83edff8b085dba6a9211c03fbb7
Teja Kiran Chadalavada <Teja.Chadalavada@hotmail.com>
2018-04-15 01:36:26 +0530

    Making it look nice by copy pasting from internet

--- Home.py
+++ Home.py
@@ -10,9 +10,22 @@ db = SQLAlchemy(app)
 
 @app.route('/dashboard')
 def dashboard():
-    labels_data = [str(val.logtime_year)+'/'+str(val.logtime_month)+'/'+str(val.logtime_date) for val in Models.daily_summary.query.limit(100).all()]
-    values_data = [float(val.kwh_ontheday) for val in Models.daily_summary.query.limit(100).all()]
-    return render_template('linechart.html',title='Last 100 days of Power usage',labels=labels_data, values=values_data,max=10)
+    #complete_lab_data = []
+    #complete_val_data = []
+    complete_data = []
+    diff_devices = Models.devices.query.limit(10).all()
+    for eachdevice in diff_devices:
+        lab_val_data = [[str(val.logtime_year)+'/'+str(val.logtime_month)+'/'+str(val.logtime_date),float(val.kwh_ontheday)] for val in Models.daily_summary.query.filter(Models.daily_summary.devices_name == eachdevice.name ).limit(100).all()]
+        max_val_set = max([val[1] for val in lab_val_data])
+        lab_val_max = [lab_val_data,[max_val_set],[eachdevice.name],[eachdevice.id]]
+        #label_data = [lab[0] for lab in lab_val_data]
+        #value_data = [val[1] for val in lab_val_data]
+
+        #complete_lab_data = complete_lab_data.append(label_data)
+        #complete_val_data = complete_val_data.append(value_data)
+        complete_data.append(lab_val_max)
+
+    return render_template('linechart.html',charts = complete_data)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug="True")
